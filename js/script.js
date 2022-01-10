b0951a99314451902e3776feba5bcc4b560106ad
sandipthapa99 <sandipthapa386@gmail.com>
2022-01-10 10:17:51 +0545

    nav responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-function myFunction() {
+// const toggleBtn = document.getElementsByClassName('toggle_btn');
+
+
+function myFunc(){
     var x = document.getElementById("navbar");
-    if (x.className === "nav_wrapper") {
-      x.className += " responsive";
+    if (x.className === "nav_ul") {
+        x.className += " nav_ul_active";
     } else {
-      x.className = "nav_wrapper";
+        x.className = "nav_ul";
     }
-  }
+}
